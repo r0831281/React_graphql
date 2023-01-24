d50d1eeb53c845ecf3a0fca76d878e5876eb2311
Jonas Quintiens <r0831281@student.thomasmore.be>
2023-01-24 18:04:50 +0100

    lesson 3 init

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 
-export default function App() {
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+
+import configData from "./config.json";
+
+const client = new ApolloClient({
+  uri: configData.qlendpoint,
+  headers: {
+    'x-hasura-admin-secret': configData.qlkey
+  },
+  cache: new InMemoryCache()
+});
+
+const GET_CONTINENTS = gql`
+  query GetContinents {
+    continents {
+      id
+      code
+      name
+    }
+  }
+`;
+
+function ListContinents() {
+  const { data, loading } = useQuery(GET_CONTINENTS);
+
+  if (loading) return <Text>Loading...</Text>
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={data.continents}
+      renderItem={({ item }) => <Text>{item.name}</Text>}
+      keyExtractor={(item, index) => index}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <ApolloProvider client={client}>
+      <View style={{ margin: 100 }}>
+        <ListContinents />
+      </View>
+    </ApolloProvider>
+  );
+}
